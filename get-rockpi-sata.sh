2a7c5959660b5c2ad07e5c8d56dce8755da7555a
Miltiadis Kallianis <kallianis@meinwatt.de>
2023-07-23 17:14:05 +0200

    update get-rockpi-sata with latest version using local files

--- get-rockpi-sata.sh
+++ get-rockpi-sata.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-AUTHOR='Akgnah <ka@vamrs.com>'
-VERSION='0.12'
+AUTHOR='Akgnah <setq@radxa.com>'
+VERSION='0.14'
 PI_MODEL=`tr -d '\0' < /proc/device-tree/model`
-RPI_DEB="https://cos.setf.me/rockpi/deb/raspi-sata-${VERSION}.deb"
+RPI_DEB="https://s3.setq.io/rockpi/deb/raspi-sata-${VERSION}.deb"
+SSD1306="https://s3.setq.io/rockpi/pypi/Adafruit_SSD1306-v1.6.2.zip"
 
 confirm() {
   printf "%s [Y/n] " "$1"
@@ -17,8 +18,8 @@ confirm() {
   return 1
 }
 
-rpi_check() {
-  packages="python3-rpi.gpio python3-pip python3-pil"
+apt_check() {
+  packages="python3-rpi.gpio python3-setuptools python3-pip python3-pil python3-spidev pigpio python3-pigpio"
   need_packages=""
 
   idx=1
@@ -38,27 +39,54 @@ rpi_check() {
   fi
 }
 
-rpi_install() {
+deb_install() {
   TEMP_DEB="$(mktemp)"
-  curl -sL "$RPI_DEB" -o "$TEMP_DEB"
+  # curl -sL "$RPI_DEB" -o "$TEMP_DEB"
+  cp files/raspi-sata-0.14.deb $TEMP_DEB
   dpkg -i "$TEMP_DEB"
   rm -f "$TEMP_DEB"
 }
 
-rpi_enable() {
+dtb_enable() {
   python3 /usr/bin/rockpi-sata/misc.py open_w1_i2c
 }
 
+pip_fixpath() {
+  path="/usr/local/lib/python$(python3 -V | cut -c8-10)/dist-packages"
+  if [ ! -d $path ]; then
+    mkdir -p $path
+  fi
+}
+
+pip_clean() {
+  packages="Adafruit-GPIO Adafruit-PureIO Adafruit-SSD1306"
+  for package in $packages; do
+    if pip3 list 2> /dev/null | grep "$package" > /dev/null; then
+      sudo -H pip3 uninstall "$package" -y
+    fi
+  done
+}
+
 pip_install() {
-  pip3 install Adafruit-SSD1306
+  pip_fixpath
+  pip_clean
+
+  TEMP_ZIP="$(mktemp)"
+  TEMP_DIR="$(mktemp -d)"
+  # curl -sL "$SSD1306" -o "$TEMP_ZIP"
+  cp files/Adafruit_SSD1306-v1.6.2.zip $TEMP_ZIP
+  unzip "$TEMP_ZIP" -d "$TEMP_DIR" > /dev/null
+  cd "${TEMP_DIR}/Adafruit_SSD1306-v1.6.2"
+  python3 setup.py install && cd -
+  rm -rf "$TEMP_ZIP" "$TEMP_DIR"
 }
 
 main() {
   if [[ "$PI_MODEL" =~ "Raspberry" ]]; then
-    rpi_check
-    rpi_install
+    apt_check
     pip_install
-    rpi_enable
+    deb_install
+    dtb_enable
   else
     echo 'nothing'
   fi
